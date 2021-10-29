e8f9c8d3b3d39c0f1733051b83c101a1475ed99a
Christopher Wolfe <chriswolfe93091@gmail.com>
2021-10-28 20:52:04 -0700

    Add files via upload

---
+++ package_install.R
@@ -0,0 +1,8 @@
+#Install the requisite packages for MI publication#
+
+install.packages("devtools")
+install.packages("tidyverse")
+install.packages("doparallel")
+install.packages("foreach")
+install.packages("mixtools")
+devtools::install_github("MichaelHoltonPrice/yada", ref="dev")
